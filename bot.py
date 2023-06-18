cd10c6025a884ac81f4c117f519f0c58b927c9ce
Kostiantyn Kriuchkov <36363097+Latand@users.noreply.github.com>
2023-06-18 07:43:09 +0300

    Initial commit

---
+++ bot.py
@@ -0,0 +1,64 @@
+import asyncio
+import logging
+
+import betterlogging as bl
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.fsm.storage.redis import RedisStorage, DefaultKeyBuilder
+
+from tgbot.config import load_config
+from tgbot.handlers import routers_list
+from tgbot.middlewares.config import ConfigMiddleware
+from tgbot.services import broadcaster
+
+logger = logging.getLogger(__name__)
+log_level = logging.INFO
+bl.basic_colorized_config(level=log_level)
+
+
+async def on_startup(bot: Bot, admin_ids: list[int]):
+    await broadcaster.broadcast(bot, admin_ids, "Бот був запущений")
+
+
+def register_global_middlewares(dp: Dispatcher, config, session_pool=None):
+    dp.message.outer_middleware(ConfigMiddleware(config))
+    dp.callback_query.outer_middleware(ConfigMiddleware(config))
+
+    # dp.message.outer_middleware(DatabaseMiddleware(session_pool))
+    # dp.callback_query.outer_middleware(DatabaseMiddleware(session_pool))
+
+
+async def main():
+    logging.basicConfig(
+        level=logging.INFO,
+        format=u'%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s',
+    )
+    logger.info("Starting bot")
+    config = load_config(".env")
+    if config.tg_bot.use_redis:
+        storage = RedisStorage.from_url(config.redis.dsn(),
+                                        key_builder=DefaultKeyBuilder(with_bot_id=True, with_destiny=True))
+    else:
+        storage = MemoryStorage()
+    bot = Bot(token=config.tg_bot.token, parse_mode='HTML')
+    dp = Dispatcher(storage=storage)
+    # engine = create_engine(config.db)
+    # session_pool = create_session_pool(engine)
+
+    dp.include_routers(*routers_list)
+
+    register_global_middlewares(
+        dp,
+        config,
+        # session_pool=session_pool,
+    )
+
+    await on_startup(bot, config.tg_bot.admin_ids)
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        logger.error("Бот був вимкнений!")
